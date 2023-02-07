663052a8931fa9764e7a7c90e210e21381c33cee
Justus Perlwitz <mail@justusperlwitz.com>
2023-02-07 14:53:01 +0900

    Allow pulling self

--- clone.py
+++ clone.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from itertools import count
 import subprocess
 from argparse import ArgumentParser
 from multiprocessing import Pool
@@ -8,34 +9,52 @@ from pathlib import Path
 import requests
 
 
+API_BASE = "https://api.github.com/"
 out_folder = Path("out")
 
 
 def update_repo(repository):
     target = repository["owner"]["login"]
     repository_name = repository["name"]
-    print("Cloning" ,repository_name)
+    print(f"Attempting to clone {repository_name}")
     clone_path = out_folder / target / repository_name
     clone_command = ("git", "clone", repository["ssh_url"], clone_path)
-    print("Running", clone_command)
+    print(f"Running f{clone_command}")
     process = subprocess.run(clone_command)
     if process.returncode:
-        fetch_command = ("git", "fetch")
-        print("Running", fetch_command, "in", clone_path)
-        subprocess.run(fetch_command, cwd=clone_path, check=True)
+        print(f"Not successful with status {process.returncode}")
+        print("Assuming already cloned and needing to pull")
+        pull_command = ("git", "pull")
+        print(f"Running {pull_command} in {clone_path}")
+        subprocess.run(pull_command, cwd=clone_path, check=True)
+        lfs_command = "git", "lfs", "pull"
+        print(f"Running {lfs_command}")
+        subprocess.run(lfs_command, cwd=clone_path, check=True)
 
 
 def main():
     access_token = environ["ACCESS_TOKEN"]
-    url = (
-        f"https://api.github.com/{args.type}/{args.target}/repos?per_page=200"
-    )
+    if args.type == "self":
+        url = f"{API_BASE}user/repos"
+    else:
+        url = (
+            f"{API_BASE}{args.type}/{args.target}/repos"
+        )
     session = requests.Session()
-    session.headers.update({"Authorization": access_token})
-    print("Requesting", url)
-    repositories = session.get(url).json()
-    print(f"Cloning or fetching {len(url)} repositories")
+    session.headers.update({"Authorization": f"Bearer {access_token}"})
 
+    repositories = []
+    for page in count(1):
+        print(f"Retrieving page # {page}")
+        page_url = f"{url}?per_page=100&page={page}"
+        print(f"Requesting {page_url}")
+        result = session.get(page_url).json()
+        print(f"Got {len(result)} repositories")
+        repositories.extend(result)
+        if len(result) < 100:
+            break
+
+    print(f"Cloning or pulling {len(repositories)} repositories")
     with Pool() as pool:
         pool.map(update_repo, repositories)
 
@@ -43,6 +62,6 @@ def main():
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument('target')
-    parser.add_argument('--type', choices=["users", "orgs"], default="users")
+    parser.add_argument('--type', choices=["self", "users", "orgs"], default="users")
     args = parser.parse_args()
     main()
